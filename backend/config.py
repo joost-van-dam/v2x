3511da9f4dc0e0eb482ba98e41a12ec0559321e1
Joost <github@joost.zip>
2025-06-17 08:32:25 +0200

    fix security ding

--- backend/config.py
+++ backend/config.py
@@ -10,10 +10,10 @@ import os
 
 class _Settings:
     # Influx
-    INFLUX_URL: str = os.getenv("INFLUX_URL", "http://localhost:8086")
-    INFLUX_TOKEN: str = os.getenv("INFLUX_TOKEN", "my-token")
-    INFLUX_ORG: str = os.getenv("INFLUX_ORG", "v2x_org")
-    INFLUX_BUCKET: str = os.getenv("INFLUX_BUCKET", "v2x_bucket")
+    INFLUX_URL: str = os.getenv("INFLUX_URL", "https://influx-playground.sendlab.nl")
+    INFLUX_TOKEN: str = os.getenv("INFLUX_TOKEN", "nUUDW9_TvfwX1vRVuUyVzW1lBiyGBoYTtrK6CyXU-l_Hn5RYbvppAsrnOdEPJn3RfoFsFRzC6DvVDNB8PggNfg==")
+    INFLUX_ORG: str = os.getenv("INFLUX_ORG", "Sendlab")
+    INFLUX_BUCKET: str = os.getenv("INFLUX_BUCKET", "CSMS_Gateway")
 
     # Postgres (asyncpg DSN)
     POSTGRES_DSN: str = os.getenv(
